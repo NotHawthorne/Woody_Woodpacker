65e18e1fce78737313f8943f4989ac686d560154
alkozma <nothawthorne@airmail.cc>
2020-03-11 14:00:12 -0700

    working on creating the patcher

--- c/infect.c
+++ c/infect.c
@@ -35,8 +35,10 @@ static int	load_file(char *fname, struct s_woody *w)
 {
 	Elf64_Shdr	*sect;
 	Elf64_Shdr	*strtab;
+	//Elf64_Shdr	new;
 	void		*tst;
 	unsigned long	new_os;
+	int		fd;
 
 	if ((w->fd = open(fname, O_RDONLY)) < 0 || (fstat(w->fd, &w->s) < 0))
 		return (0);
@@ -50,12 +52,25 @@ static int	load_file(char *fname, struct s_woody *w)
 	new_os = 0;
 	for (int i = 0; i != w->header->e_shnum; i++)
 	{
-		printf("%lu\n", sect->sh_offset - new_os);
 		new_os = (sect->sh_offset + sect->sh_size);
-		printf("%s %lu\n", (char*)(strs + sect->sh_name), sect->sh_offset);
+		printf("%s %lu %ld\n", (char*)(strs + sect->sh_name), sect->sh_offset, sect->sh_addr);
 		sect++;
 	}
-	printf("PUT NEW SECTION HEADER AT OFFSET %lu\n", new_os);
+	w->header->e_entry = w->header->e_shentsize + 8;
+	w->header->e_shoff += _BUFFER_SIZE;
+
+	fd = open("new", O_CREAT | O_RDWR);
+	write(fd, (void*)w->header, w->header->e_entry);
+	write(fd, buffer, _BUFFER_SIZE);
+	write(fd, ((void*)w->header) + w->header->e_entry, w->s.st_size - w->header->e_entry);
+	printf("%ld %ld\n", w->s.st_size, w->header->e_entry);
+	close(fd);
+	/*new.sh_name = 0x0;
+	new.sh_type = SHT_PROGBITS;
+	new.sh_flags = SHF_EXECINSTR;
+	new.sh_addr = sect->sh_addr + sect->sh_size;
+	new.sh_offset*/
+	printf("PUT NEW SECTION HEADER AT OFFSET %lu\n", w->header->e_entry);
 	return (1);
 }
 
